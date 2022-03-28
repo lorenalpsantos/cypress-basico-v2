6078aa67a33ebea3a5dbc670db7388c6691aa0c9
Lorena Lopes <lorenalpsantos@gmail.com>
2022-03-28 15:56:35 -0300

    ok

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -2,15 +2,16 @@
 
 
 describe('Central de Atendimento ao Cliente TAT', function(){
-    beforeEach(() => {
-        it('verifica o título da aplicação',function(){
-        cy.visit('./src/index.html')
-        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
-        
+    beforeEach(function() {
+        cy.visit('./src/index.html')      
         })
+
+    it('verifica o título da aplicação',function(){
+        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
+        
     it('preenche os campos obrigatórios e envia o formulário', function(){
-        cy.get('input[id="firstName"]').type('Lorena')
+        cy.get('#firstName').type('Lorena')
        /* cy.get('//*[@id="lastName"]').type('Lopes')
         cy.get('//*[@id="email"]').type('lorenalpsantos@gmail.com')
         cy.get('//*[@id="open-text-area"]').type('Olá! Gostaria de mais informações sobre o curso.')
